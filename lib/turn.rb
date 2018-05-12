d2fc737e97f92b1539cd79542dd2f2f7a4a67194
Josh Daniell <josh.daniell@wework.com>
2018-05-12 15:51:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,14 +35,12 @@ end
 
 def move (board, index, char = "X")
   board[index] = char
-  # return board
 end 
 
 def turn(board)
+  
   puts "Please enter 1-9:"
-  
   num = gets.chomp 
-  
   index = input_to_index(num)
   
   if valid_move?(board, index)
